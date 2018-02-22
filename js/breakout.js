478dba08e726a96e37cbccee62416697a8a7a6c2
deanmcdonagh <36044271+deanmcdonagh@users.noreply.github.com>
2018-02-22 16:26:14 +0000

    Moving ball updated

--- js/breakout.js
+++ js/breakout.js
@@ -10,14 +10,20 @@ var dx = 2
 var dy = -2
 
 //Draw the ball
-function draw() {
+function drawBall() {
 	ctx.beginPath();
 	ctx.arc(x, y, 10, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
-	x +=dx;
-	y +=dy;
+}
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
+	x += dx;
+	y += dy;
 }
 
 setInterval(draw, 10);
+
