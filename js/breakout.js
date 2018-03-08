066d78847534bd774726a18fb7081a38d24d11b5
deanmcdonagh <36044271+deanmcdonagh@users.noreply.github.com>
2018-03-08 16:11:05 +0000

    Draw Bricks

--- js/breakout.js
+++ js/breakout.js
@@ -13,6 +13,25 @@ var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 
+//Setup some bricks
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+//Hold the bricks in a two dimensional array - think of it as rows and columns 
+var bricks = [];
+for(c=0; c<brickColumnCount; c++) {
+	bricks[c] = [];
+	for(r=0; r<brickRowCount; r++) {
+		bricks[c] [r] = { x: 0, y: 0, status: 1};
+	}
+}
+
+
 
 
 
@@ -44,6 +63,25 @@ function drawPaddle() {
 	ctx.closePath();
 }
 
+//This function draws the bricks
+function drawBricks() {
+	for(c=0; c<brickColumnCount; c++) {
+		for(r=0; r<brickRowCount; r++) {
+			var brickX = (c*(brickWidth+brickPadding) )+brickOffsetLeft;
+			var brickY = (r*(brickHeight+brickPadding) )+brickOffsetTop;
+			bricks[c][r].x = brickX;
+			bricks[c][r].y = brickY;
+			ctx.beginPath();
+			ctx.rect(brickX, brickY, brickWidth, brickHeight);
+			ctx.fillStyle = "#0095DD";
+			ctx.fill();
+			ctx.closePath();
+		}
+	}
+}
+
+
+
 
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -51,7 +89,8 @@ function draw() {
 
 drawPaddle();
 
-
+//draw the bricks
+drawBricks();
 
 	x += dx;
 	y += dy;
@@ -63,9 +102,19 @@ drawPaddle();
 	if(y + dy < ballRadius){
 		dy = -dy;
 	} else if(y + dy > canvas.height-ballRadius) {
+		//Check if the ball is hitting the paddle
+		if(x > paddleX && x < paddleX + paddleWidth) {
+			dy = -dy;
+		}
+		else{ 
 		alert("Game Over");
 		document.location.reload();
 	}
+	
+	
+}
+
+	
 	
 	
 	if(rightPressed && paddleX < canvas.width-paddleWidth){ 
