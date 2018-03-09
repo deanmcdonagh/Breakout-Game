235ed422eee7f61cd2c4a14f06e143ed20798780
deanmcdonagh <36044271+deanmcdonagh@users.noreply.github.com>
2018-03-09 10:27:38 +0000

    Display Winning Message

--- js/breakout.js
+++ js/breakout.js
@@ -178,6 +178,11 @@ function collisionDetection() {
 				dy = -dy;
 				b.status = 0;
 				score++;
+				if(score == brickRowCount*brickColumnCount) {
+					alert("YOU WIN, CONGRATULATIONS!");
+					document.location.reload();
+				}
+				
 				}
 			
 			}
